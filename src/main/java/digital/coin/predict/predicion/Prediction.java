466163ff1c8a5b326ec7ebdfedb6370497706993
kwonsw812 <kwonsw812@gmai.com>
2024-01-20 21:03:32 +0900

    git commit

--- src/main/java/digital/coin/predict/predicion/Prediction.java
+++ src/main/java/digital/coin/predict/predicion/Prediction.java
@@ -11,6 +11,6 @@ public class Prediction {
         WebClient webClient = WebClient.builder().build();
         String url = "http://127.0.0.1:5000/prediction";
 
-        webClient.get().uri(url).retrieve().bodyToMono(String.class).block();
+        webClient.get().uri(url).retrieve().bodyToMono(Void.class).block();
     }
 }
